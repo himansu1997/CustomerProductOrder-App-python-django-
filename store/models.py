00aa7c6ff0dcd7d5cb3e7bc203a23c5ce4d50616
Himansu Sethi <hsekhar29@gmail.com>
2020-09-25 23:27:48 +0530

    added vendor model

--- store/models.py
+++ store/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Vendor(models.Model):
+    vendor_store_name = models.CharField(max_length=200, null=True, blank=True)
+    vendor_store_number = models.CharField(max_length=20, null=True, blank=True)
+    vendor_store_email = models.CharField(max_length=20, null=20, blank=True)
+    vendor_store_address = models.CharField(max_length=200, null=True, blank=True)
+
 class Product(models.Model):
+    vendor = models.ForeignKey(Vendor)
     product_number = models.CharField(max_length=100,unique=True)
     name = models.CharField(max_length=250)
     brand = models.CharField(max_length=250, blank=True)
@@ -10,6 +17,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=15, decimal_places=2, default=0.0)
     created = models.DateTimeField(auto_now_add=True,null=True)
     modified = models.DateTimeField(auto_now=True,null=True)
+    product_category = models.CharField(max_length=250, blank=True) 
 
     def __str__(self):
         return "%s"%self.product_number
